8f0f4bc611fe505b4e591dc184ff3a79043e3dbb
Aaksnksha S <aakanksha@dgsol-in.com>
2017-07-22 18:59:18 +0530

    Sampling Request Updated for new requirements

--- tfaddon/back_office/doctype/sampling_request/sampling_request.js
+++ tfaddon/back_office/doctype/sampling_request/sampling_request.js
@@ -29,23 +29,3 @@ frappe.ui.form.on('Sampling Request', {
 });
 
 
-cur_frm.cscript.show_hide_fields = function(doc) {
-	main_fields = ['transaction_date','sales_order','assigned_to','contact_name','contact_no'];
-	request_fields = ['est_samples','est_container','req_remarks'];
-	response_fields = ['act_samples','act_bottles','response_remarks'];
-	if (doc.req_status == 'NEW') {
-		hide_field(response_fields);
-		toggle_reqd(['assigned_to','contact_name','contact_no','est_samples','est_container'],true);
-		toggle_enable(main_fields,false);
-		toggle_enable(request_fields,false);
-	} else if (doc.req_status == 'ASSIGNED') {
-		unhide_field(response_fields);
-		toggle_reqd(['act_samples','act_bottles'],true);
-		toggle_reqd(['assigned_to','contact_name','contact_no','est_samples','est_container'],false);
-		toggle_enable(main_fields,true);
-		toggle_enable(request_fields,true);
-	} else {
-		unhide_field(response_fields);
-
-	}
-}
--- tfaddon/back_office/doctype/sampling_request/sampling_request.py
+++ tfaddon/back_office/doctype/sampling_request/sampling_request.py
@@ -20,37 +20,29 @@ class SamplingRequest(Document):
 	def validate_mandatory_field(self):
 		msg = ''
 		today = date.today()
-		if self.req_status == 'New':
+		if (self.status == 'Draft' or self.status == 'Assigned'):
 			# validate transaction date v/s today's date
 			if self.est_start_date:
 				if getdate(self.est_start_date) < getdate(self.transaction_date):
 					frappe.throw(_("Start Date cannot be past date"))
 
 			# validate other mandatory field
-			if not self.site_location:
-				frappe.throw(_("Site Location is Required"))
-			if not self.contact_name:
-				frappe.throw(_("Person to Contact is Required"))
-			if not self.contact_no:
-				frappe.throw(_("Contact No is Required"))
 			if not self.est_start_date:
 				frappe.throw(_("Start Date is Required"))
 			if not self.est_duration:
 				frappe.throw(_("Activity Duration is Required"))
+			if not self.contact_name:
+				frappe.throw(_("Person to Contact is Required"))
+			if not self.contact_no:
+				frappe.throw(_("Contact No is Required"))
+			if not self.site_location:
+				frappe.throw(_("Site Location is Required"))
 			if not self.est_samples:
 				frappe.throw(_("Estimated No of Samples is Required"))
 			if not self.est_container:
 				frappe.throw(_("Estimated No of Bottles is Required"))
-
-		if self.req_status == 'Assigned':
 			if not self.assigned_to:
 				frappe.throw(_("Job Assigned to is Required"))
-
-		if self.req_status == 'Closed':
-			if msg: 
-				msg = msg + "are required to proceed"
-				frappe.throw(_(msg))
-
 
 	def update_readonly_fields(self):
 		pass
